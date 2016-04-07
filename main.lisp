6cf8dad3d2c561b5872aa54b71d6d48c5583ca74
Alex Dunn <dunn.alex@gmail.com>
2016-04-07 10:24:08 -0700

    [ron paul voice] its happenening

--- main.lisp
+++ main.lisp
@@ -1,3 +1,20 @@
 (load "quicklisp/setup.lisp")
 (ql:quickload 'hunchentoot)
-(hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4444))
+
+(defvar party (make-instance
+               'hunchentoot:easy-acceptor
+                :port 3000
+                :document-root "www/"
+                :access-log-destination "log/access.log"
+                :message-log-destination "log/error.log"))
+
+(setq hunchentoot:*dispatch-table*
+      (list
+       (hunchentoot:create-regex-dispatcher "^/toot" 'toot)))
+
+(defun toot ()
+  "Toot"
+  (setf (hunchentoot:content-type*) "text/plain")
+  (format nil "fart"))
+
+(hunchentoot:start party)
