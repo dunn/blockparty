5f162cd2a69c6467d5a496379903099f8fb6b1cc
Alex Dunn <dunn.alex@gmail.com>
2016-07-03 20:54:43 -0700

    rename block-party => blockparty

--- lib/handlers.lisp
+++ lib/handlers.lisp
@@ -1,19 +1,20 @@
 ;; Copyright 2016 Alex Dunn <dunn.alex@gmail.com>
 
-;; This file is part of block-party.
+;; This file is part of blockparty.
 
-;; block-party is free software: you can redistribute it and/or modify
+;; blockparty is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published
 ;; by the Free Software Foundation, either version 3 of the License,
 ;; or (at your option) any later version.
 
-;; block-party is distributed in the hope that it will be useful, but
+;; blockparty is distributed in the hope that it will be useful, but
 ;; WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with read-thing.  If not, see <http://www.gnu.org/licenses/>.
+;; along with blockparty.  If not, see <http://www.gnu.org/licenses/>.
+
 
 (defun login ()
   "Not implemented."
